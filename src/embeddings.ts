0a509f58cd0ac6d11fdf1ae3a644245dfb53219d
Riza Fahmi <rizafahmi@gmail.com>
2025-11-04 14:20:16 +0700

    Semantic search using transformers.js

---
+++ src/embeddings.ts
@@ -0,0 +1,22 @@
+import { pipeline } from "@huggingface/transformers";
+
+export async function embedText(text: string) {
+  const embeddingPipeline = await pipeline(
+    "embeddings",
+    "TaylorAI/bge-micro-v2",
+  );
+  const { data } = await embeddingPipeline(text, {
+    pooling: "mean",
+    normalize: true,
+  });
+  return data;
+}
+
+export function cosineSimilarity(vec1: Array<number>, vec2: Array<number>) {
+  let result = 0;
+  for (let i = 0; i < vec1.length; i++) {
+    result += vec1[i] * vec2[i];
+  }
+
+  return result;
+}
--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,24 +1,29 @@
 import type { APIRoute } from "astro";
-import { db, Venue, like } from "astro:db";
+import { db, like, or, Venue } from "astro:db";
 
 export const GET: APIRoute = async ({ url }) => {
-	const query = url.searchParams.get("q")?.trim();
+  const query = url.searchParams.get("q")?.trim();
 
-	if (!query) {
-		const venues = await db.select().from(Venue);
-		return new Response(JSON.stringify(venues), {
-			status: 200,
-			headers: { "Content-Type": "application/json" },
-		});
-	}
+  if (!query) {
+    const venues = await db.select().from(Venue);
+    return new Response(JSON.stringify(venues), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
-	const venues = await db
-		.select()
-		.from(Venue)
-		.where(like(Venue.name, `%${query}%`));
+  const venues = await db
+    .select()
+    .from(Venue)
+    .where(
+      or(
+        like(Venue.name, `%${query}%`),
+        like(Venue.description, `%${query}%`)
+      )
+    );
 
-	return new Response(JSON.stringify(venues), {
-		status: 200,
-		headers: { "Content-Type": "application/json" },
-	});
+  return new Response(JSON.stringify(venues), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 };
