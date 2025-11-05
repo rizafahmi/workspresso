6a385d9893cc14077559ee5d30418c12f6596791
Riza Fahmi <rizafahmi@gmail.com>
2025-11-05 10:08:30 +0700

    Implement auto tagging feature using classification

--- src/ai.ts
+++ src/ai.ts
@@ -1,6 +1,10 @@
+import { pipeline } from "@huggingface/transformers";
+
 type Result = { status: "ok" | "ko"; text?: string; error?: string };
+
 export async function generate(
-  prompt: string, config: Record<string, unknown> = {},
+  prompt: string,
+  config: Record<string, unknown> = {},
 ): Promise<Result> {
   const url =
     `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent`;
@@ -22,9 +26,7 @@ export async function generate(
       }),
     });
 
-    console.log(result);
     const { candidates } = await result.json();
-    console.log(candidates);
     const text = candidates[0].content.parts[0];
     return { status: "ok", text };
   } catch (err) {
@@ -33,4 +35,28 @@ export async function generate(
       error: (err as Error).message,
     };
   }
+}
+
+export async function generateTags(text: string) {
+  const classifier = await pipeline(
+    "zero-shot-classification",
+    "cross-encoder/nli-MiniLM2-L6-H768",
+  );
+
+  await classifier("warm up", ["a", "b", "c"]);
+  const candidateLabels = [
+    "WFC",
+    "Cafe",
+    "Quiet",
+    "Loud",
+    "Cozy",
+    "Spacious",
+    "Fast WiFi",
+    "Good for Work",
+    "Great Coffee",
+    "Crowded",
+  ];
+  const result = await classifier(text, candidateLabels);
+  console.log(result);
+  return result;
 }
