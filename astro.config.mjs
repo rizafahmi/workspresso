a9b40d23a8b3d1e25dae4c7b437f4200d53bf5d9
Riza Fahmi <rizafahmi@gmail.com>
2025-11-15 07:41:28 +0700

    Add persistence db and nodejs adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import db from "@astrojs/db";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -13,4 +15,8 @@ export default defineConfig({
 
   output: "server",
   integrations: [db({ mode: "node" })],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
