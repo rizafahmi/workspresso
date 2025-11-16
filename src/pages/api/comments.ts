76bd3fb4149d06cb04325b55fb7794f843812373
Riza Fahmi <rizafahmi@gmail.com>
2025-11-17 06:18:20 +0700

    Soft-remove summarize comments

--- src/pages/api/comments.ts
+++ src/pages/api/comments.ts
@@ -34,6 +34,7 @@ export async function POST({ request }: { request: Request }) {
       imageUrl: imageData ? imageData : null,
     });
 
+    /* TODO: ⚠️ Summarize comments ⚠️ */
     updateCommentsSummary(venueId);
 
     return new Response(
@@ -55,41 +56,42 @@ export async function POST({ request }: { request: Request }) {
   }
 }
 
-async function updateCommentsSummary(venueId) {
-  const comments = await db.select().from(Comment).where(
-    eq(Comment.venueId, Number(venueId)),
-  );
-  const prompt =
-    `You are part of customer experience system. Your job is to write brief summaries of customer's comments and interactions. This is to help support agents to understand the context quickly so they can help the venue owner efficiently.
+/* TODO: ⚠️ Summarize comments function ⚠️ */
+// async function updateCommentsSummary(venueId) {
+//   const comments = await db.select().from(Comment).where(
+//     eq(Comment.venueId, Number(venueId)),
+//   );
+//   const prompt =
+//     `You are part of customer experience system. Your job is to write brief summaries of customer's comments and interactions. This is to help support agents to understand the context quickly so they can help the venue owner efficiently.
 
-  The comments so far is:
+//   The comments so far is:
 
-  ${commentList(comments)}
+//   ${commentList(comments)}
 
-  Write a 10-word summary of all the things CUSTOMER has said. Then based on that summary, score the customer's satisfaction using one of the following phrases ranked from worst to best: worst, bad, ok, good, great, awesome.
-  Pay particular attention to the TONE of the customer's message, as we are most interested in their emotional state.
-  `;
-  console.log(prompt);
-  const config = {
-    "responseMimeType": "application/json",
-    "responseSchema": {
-      type: "OBJECT",
-      properties: {
-        summary: { type: "STRING" },
-        satisfaction: { type: "STRING" },
-      },
-    },
-  };
-  const { status, text } = await generate(prompt, config);
-  const { summary, satisfaction } = JSON.parse(text);
-  console.log({ summary, satisfaction });
+//   Write a 10-word summary of all the things CUSTOMER has said. Then based on that summary, score the customer's satisfaction using one of the following phrases ranked from worst to best: worst, bad, ok, good, great, awesome.
+//   Pay particular attention to the TONE of the customer's message, as we are most interested in their emotional state.
+//   `;
+//   console.log(prompt);
+//   const config = {
+//     "responseMimeType": "application/json",
+//     "responseSchema": {
+//       type: "OBJECT",
+//       properties: {
+//         summary: { type: "STRING" },
+//         satisfaction: { type: "STRING" },
+//       },
+//     },
+//   };
+//   const { status, text } = await generate(prompt, config);
+//   const { summary, satisfaction } = JSON.parse(text);
+//   console.log({ summary, satisfaction });
 
-  await db
-    .update(Venue)
-    .set({ summary, satisfaction })
-    .where(eq(Venue.id, Number(venueId)))
-    .run();
-}
+//   await db
+//     .update(Venue)
+//     .set({ summary, satisfaction })
+//     .where(eq(Venue.id, Number(venueId)))
+//     .run();
+// }
 
 function commentList(comments) {
   return comments.map((comment) => comment.comment).join(".\n");
